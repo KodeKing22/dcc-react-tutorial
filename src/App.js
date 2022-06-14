ac076edbd26cf2959897e5945404ef2979bb2a39
Jonathan Marshall <jonkylemars@yahoo.com>
2022-06-14 16:45:38 -0500

    Completed EntryChartTracker Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/Display Entries/DisplayEntries';
-import AddEntryForm from './Components/Display Entries/AddEntry/AddEntryForm';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
-  const [entries, setEntries] = useState([{weight: 175, date: "11-23-2021"}, {weight: 174, date: "11-24-2021"}])
+  const [entries, setEntries] = useState([{weight: 174, date: "11-23-2021"}, {weight: 175, date: "11-24-2021"}])
   
   function addNewEntry(entry){
     
@@ -17,6 +18,7 @@ function App() {
     <div >
       <DisplayEntries parentEntries={entries} />
       <AddEntryForm addNewEntryProperty={addNewEntry} />
+      <EntriesChartTracker parentEntries={entries}/>
      </div>
   );
 }
